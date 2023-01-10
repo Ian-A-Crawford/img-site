aae5c70f0b93e71e96cbd613d7cd3695168c0ed5
Ian Crawford <icrawford1408@gmail.com>
2023-01-09 18:28:45 -0800

    initial commit

--- server/index.js
+++ server/index.js
@@ -13,15 +13,8 @@ app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, '../imgccpy/build')));
 
-//app.use(upload.single());
-
-
-
-
-
-
 async function upload(file, name) {
-  img = file;//await read();
+  img = file;
 
 
   var params = {
@@ -32,35 +25,15 @@ async function upload(file, name) {
   };
   s3.putObject(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data); // successful response
-    /*
-    data = {
-     ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
-     VersionId: "Bvq0EDKxOcXLJXNo_Lkz37eM3R4pfzyQ"
-    }
-    */
+    else console.log(data); // successful responseS
   });
 }
 
-
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!') //placeholder, will hold UI
 })
-let newImages = []
 
 app.post('/create', upload.single('file'), (req, res) => {
-  //  var newImg = {
-  //     "url": req.body.url,
-  //  }
-  //  newImages.push(newImg);
-  //  // Here will be a call to the read function in the test.js file, 
-  //  // adding the image to S3
-  //  console.log(newImages);
-  console.log(req.file.buffer);
   upl(req.file.buffer, req.file.originalname)
 
 })
